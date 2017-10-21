f79d9b282ec95a51dfdb1ff7e1fd220f4222e421
Marian Serna <marianhalliday@gmail.com>
2017-10-21 12:31:41 -0400

    Appointment create action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+    User.find(session[:user_id]) if logged_in?
+  end
+
+  # make current user available in views
+  helper_method :current_user
+
+  # is there a value in session id
+  def logged_in?
+    session[:user_id].present?
+  end
+
+  helper_method :logged_in?
 end
--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -9,7 +9,16 @@ class AppointmentsController < ApplicationController
   end
 
   def create
-    
+    @appointment = Appointment.new(appointment_params)
+    # which user is making the appointment? -> current_user comes from session
+    @appointment.user_id = current_user.id
+
+    if @appointment.save
+      flash[:notice] = "Your appointment has been saved"
+      redirect_to appointments_url
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,6 +27,12 @@ class AppointmentsController < ApplicationController
 
   def update
 
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:dentist_id, :appointment_at)
   end
 
 end
